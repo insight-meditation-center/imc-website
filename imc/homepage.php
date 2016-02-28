fa69857550be113ad85dc7094abb8f857ac39fa7
Ryan Oblak <rroblak@gmail.com>
2016-02-28 14:35:16 -0800

    Clean up homepage.

--- imc/homepage.php
+++ imc/homepage.php
@@ -2,7 +2,8 @@
 /**
  * Template Name: Home Page
  *
- * This is the template that displays all pages by default.
+ * This is the home page template.
+ *
  * Please note that this is the WordPress construct of pages
  * and that other 'pages' on your WordPress site may use a
  * different template.
@@ -24,25 +25,10 @@ get_header(); ?>
 
 					get_template_part( 'template-parts/content', 'home' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-
 				endwhile; // End of the loop.
 				?>
 
 			</main><!-- #main -->
-			
-			<div class="clear"></div>
-			
-			<?php if ( function_exists('dynamic_sidebar') ) { ?>
-			<aside id="home_widgets" class="col-md-12">
-				<div class="well">
-					<?php dynamic_sidebar( 'sidebar-home' ); ?>
-				</div>
-			</aside><!-- #home_widgets -->
-			<?php } ?>
 		
 		</div>
 	</section><!-- #primary -->
