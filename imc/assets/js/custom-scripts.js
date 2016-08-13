9222d99aadb79cb19b4dbe2cdcb3a8eac09787b3
Ryan Oblak <rroblak@gmail.com>
2016-08-12 17:28:08 -0700

    Remove errant js console log.

--- imc/assets/js/custom-scripts.js
+++ imc/assets/js/custom-scripts.js
@@ -8,6 +8,5 @@
 	$('[data-toggle="dropdown"], .dropdown-toggle').dropdown();
 
   // Add an offset column to the first footer widget column
-  console.log('Adding a class...' + $('#footer-widgets .widget').length);
   $('#footer-widgets .widget').first().addClass('col-md-offset-1');
 })(jQuery);
